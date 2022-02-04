86a8c89af9e78592c5b325847efea5a768c643ba
fjdeveloper <fjdeveloper@gmail.com>
2022-02-04 14:04:30 +0000

    tercero

--- app/routes/schedule.jsx
+++ app/routes/schedule.jsx
@@ -3,11 +3,22 @@ import { json, useLoaderData} from "remix"
 export const loader = async () => {
   // se ejecuta en el server. Es un http proxy
   // observar que no usa await y usa fetch (se supone que no está soportado en node.js)
+  /*
   return fetch('https://icanhazdadjoke.com/', {
     headers: {
       Accept: 'text/plain',
     },
   })
+  */
+
+  // otra forma. Le decimos en la cabecera lo que esperamos.
+  const data = await fetch('https://icanhazdadjoke.com/',{
+    headers: {
+      Accept: 'text/plain',
+    },
+  }).then(res => res.text())
+
+  return data
 }
 export default function Schedule() {
   const data = useLoaderData()
